96bf4c5f938399ec3e276b6f3ed61a06f61c7412
Santiago Rivarola <srivarola@gmail.com>
2023-11-30 20:14:05 -0300

    sess

--- src/config/session/factory.js
+++ src/config/session/factory.js
@@ -16,7 +16,7 @@ switch (args.persistance) {
                 retries: 0
             }),
             secret: env.SECRET_SESSION,
-            resave: false,
+            resave: true,
             saveUninitialized: true
         });
         break;
