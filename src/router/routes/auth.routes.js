8c3c94b7289660980fc7d7ac36789d32a68e5532
Santiago Rivarola <srivarola@gmail.com>
2023-12-01 19:54:58 -0300

    cookie

--- src/router/routes/auth.routes.js
+++ src/router/routes/auth.routes.js
@@ -55,6 +55,7 @@ export default class AuthRouter extends MyRouter {
                     let userData = new loginDto(req.user)
                     return res
                       .status(200)
+                      .cookie('apdcc_token', req.session.token, { maxAge: 60*60*24*7*1000, httpOnly: true, sameSite: 'lax' })
                       .json({
                         success: true,
                         user: userData,
