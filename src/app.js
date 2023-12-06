fdb147ec36bd1316ab6fb1637a925925a2f33145
Santiago Rivarola <srivarola@gmail.com>
2023-12-06 13:49:07 -0300

    origin

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const corsOptions = {
   credentials: true, // this allows to send back (to client) cookies
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
   // origin: "http://localhost:5173",
-  origin: "https://apdcc-app.vercel.app/",
+  origin: "https://apdcc-app.vercel.app",
   preflightContinue: false,
 };
 server.use(cors(corsOptions));
