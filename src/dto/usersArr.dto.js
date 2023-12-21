41454b8310e47b7f427139232f385e5663bc3488
Santiago Rivarola <srivarola@gmail.com>
2023-12-21 15:36:13 -0300

    user dto

--- src/dto/usersArr.dto.js
+++ src/dto/usersArr.dto.js
@@ -1,14 +1,14 @@
 export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
-            const { first_name, last_name, mail, role, country, _id } = item
+            const { first_name, last_name, mail, role, country_id, _id } = item
             const newItem = {
                 _id,
                 first_name,
                 last_name,
                 mail,
                 role,
-                country
+                country_id
             }
             return newItem
         })
