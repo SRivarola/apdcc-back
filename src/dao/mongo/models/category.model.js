19b695041629914b3aaeb2b4374810222cc4de85
Santiago Rivarola <srivarola@gmail.com>
2024-01-06 16:50:59 -0300

    categories model

--- src/dao/mongo/models/category.model.js
+++ src/dao/mongo/models/category.model.js
@@ -3,7 +3,8 @@ import { model, Schema } from 'mongoose';
 let collection = 'categories';
 
 let schema = new Schema({
-    name: { type: String, required: true, unique: true }
+    name: { type: String, required: true, unique: true },
+    sport: [{ type: String, enum: ["HOCKEY", "F7", "F11"], required: true }]
 });
 
 let Category = model(collection, schema);
