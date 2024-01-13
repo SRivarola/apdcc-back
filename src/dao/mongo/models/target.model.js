19bf34d2e105262f280315d5662e2c69ede9fccb
Santiago Rivarola <srivarola@gmail.com>
2024-01-13 16:19:10 -0300

    build

--- src/dao/mongo/models/target.model.js
+++ src/dao/mongo/models/target.model.js
@@ -1,19 +1,19 @@
-import { model, Schema, Types } from 'mongoose';
+import { model, Schema, Types } from "mongoose";
 
-const collection = 'targets';
+const collection = "targets";
 
 const schema = new Schema({
-       red_card: { type: Number, default: 0 },
-       yellow_card: { type: Number, default: 0 },
-       points: { type: Number, default: 0 },
-       wins: { type: Number, default: 0 },
-       losses: { type: Number, default: 0 },
-       ties: { type: Number, default: 0 },
-       played_matches: { type: Number, default: 0 },
-       message: { type: String, default: '' },
-       tournament_id: { type: Types.ObjectId, ref: "tournaments", required: true },
-       team_id: { type: Types.ObjectId, ref: "teams", required: true },
-       match_id: { type: Types.ObjectId, ref: "matches", required: true }
+  red_card: { type: Number, default: 0 },
+  yellow_card: { type: Number, default: 0 },
+  points: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  ties: { type: Number, default: 0 },
+  played_matches: { type: Number, default: 0 },
+  message: { type: String, default: "" },
+  tournament_id: { type: Types.ObjectId, ref: "tournaments", required: true },
+  team_id: { type: Types.ObjectId, ref: "teams", required: true },
+  match_id: { type: Types.ObjectId, ref: "matches", required: true },
 });
 
 const Target = model(collection, schema);
