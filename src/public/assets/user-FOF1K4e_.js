1df7a1d24bbfd5d931763ed918c5029230847edd
Santiago Rivarola <srivarola@gmail.com>
2024-01-15 18:39:24 -0300

    public

--- src/public/assets/user-FOF1K4e_.js
+++ src/public/assets/user-FOF1K4e_.js
@@ -1,4 +1,4 @@
-import{O as e}from"./index-BM1aPcbl.js";/**
+import{O as e}from"./index-Vg_oURG9.js";/**
  * @license lucide-react v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
