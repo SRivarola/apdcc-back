074a41fdecb3fe84554a2c768967da098ac26ccd
Santiago Rivarola <srivarola@gmail.com>
2024-01-16 18:38:53 -0300

    fix number update by round

--- src/router/routes/matches.routes.js
+++ src/router/routes/matches.routes.js
@@ -44,7 +44,7 @@ export default class MatchesRouter extends MyRouter {
     
                     for (let i= 0; i < matches.length; i++) {
                         const { date, _id } = matches[i];
-                        const newDate = moment(date).add(8, 'days');
+                        const newDate = moment(date).add(7, 'days');
                         
                         const data = { date: newDate }
                         const res = await controller.updateById(_id, data);
