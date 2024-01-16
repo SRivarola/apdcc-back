720cf43aa426e7eeac24e28db1ff850421519077
Santiago Rivarola <srivarola@gmail.com>
2024-01-16 20:00:32 -0300

    build

--- src/public/user-JRf9hmbB.js
+++ src/public/user-JRf9hmbB.js
@@ -1,4 +1,4 @@
-import{O as e}from"./index-00HffD6l.js";/**
+import{O as e}from"./index-myqaJXS9.js";/**
  * @license lucide-react v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
