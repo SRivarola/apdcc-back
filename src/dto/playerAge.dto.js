825f6490ce00efbc96826c7948c0efbd20ea78a1
Santiago Rivarola <srivarola@gmail.com>
2024-01-17 10:44:43 -0300

    player dto

--- src/dto/playerAge.dto.js
+++ src/dto/playerAge.dto.js
@@ -1,7 +1,7 @@
 export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
-            const { _id, first_name, last_name, genre, dni, dni_photo, face_photo, year, country_id, state, team } = item;
+            const { _id, first_name, last_name, genre, dni, dni_photo, face_photo, year, country_id, state, team, avatar } = item;
             const newItem = {
                 _id,
                 first_name,
@@ -14,6 +14,7 @@ export default class {
                 country_id,
                 state,
                 team,
+                avatar,
                 age: new Date().getFullYear() - year
             }
             return newItem
