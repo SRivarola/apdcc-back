0e88f78c3c677ba25e14edc6050b14380dd0abf2
Santiago Rivarola <srivarola@gmail.com>
2024-01-24 19:31:15 -0300

    fix: add prop phone in userDto

--- src/dto/usersArr.dto.js
+++ src/dto/usersArr.dto.js
@@ -1,14 +1,15 @@
 export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
-            const { first_name, last_name, mail, role, country_id, _id } = item
+            const { first_name, last_name, mail, role, country_id, _id, phone } = item
             const newItem = {
                 _id,
                 first_name,
                 last_name,
                 mail,
                 role,
-                country_id
+                country_id, 
+                phone
             }
             return newItem
         })
