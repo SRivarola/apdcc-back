1adcf157cc363e597a41c421f69c04163136c2f6
Santiago Rivarola <srivarola@gmail.com>
2024-01-24 19:41:04 -0300

    fix: add phone userDto

--- src/dto/usersArr.dto.js
+++ src/dto/usersArr.dto.js
@@ -2,6 +2,9 @@ export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
             const { first_name, last_name, mail, role, country_id, _id, phone } = item
+            const first = cadena.slice(0, 3);
+            const second = cadena.slice(3, 7);
+            const third = cadena.slice(7);
             const newItem = {
                 _id,
                 first_name,
@@ -9,7 +12,7 @@ export default class {
                 mail,
                 role,
                 country_id, 
-                phone
+                phone: `${first}-${second}-${third}`
             }
             return newItem
         })
