244e66ef521ec38217427522e668c350aee77ce3
Santiago Rivarola <srivarola@gmail.com>
2024-01-24 20:01:40 -0300

    userDto

--- src/dto/usersArr.dto.js
+++ src/dto/usersArr.dto.js
@@ -2,17 +2,27 @@ export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
             const { first_name, last_name, mail, role, country_id, _id, phone } = item
-            const first = phone.toString().slice(0, 3);
-            const second = phone.toString().slice(3, 7);
-            const third = phone.toString().slice(7);
-            const newItem = {
-                _id,
-                first_name,
-                last_name,
-                mail,
-                role,
-                country_id, 
-                phone: `${first}-${second}-${third}`
+            let newItem
+            if (phone){
+                let phone_str = phone.toString();
+                newItem = {
+                  _id,
+                  first_name,
+                  last_name,
+                  mail,
+                  role,
+                  country_id,
+                  phone: `${phone_str.slice(0,3)}-${phone_str.slice(3,7)}-${phone_str.slice(7)}`,
+                };
+            } else{
+                newItem = {
+                    _id,
+                    first_name,
+                    last_name,
+                    mail,
+                    role,
+                    country_id
+                }
             }
             return newItem
         })
