528a2030f57cdb5008ba70bf72602de07de96489
Santiago Rivarola <srivarola@gmail.com>
2024-01-24 19:43:15 -0300

    fix: userDto

--- src/dto/usersArr.dto.js
+++ src/dto/usersArr.dto.js
@@ -2,9 +2,9 @@ export default class {
     constructor(arr) {
         this.arr = arr.map((item) => {
             const { first_name, last_name, mail, role, country_id, _id, phone } = item
-            const first = cadena.slice(0, 3);
-            const second = cadena.slice(3, 7);
-            const third = cadena.slice(7);
+            const first = phone.toString().slice(0, 3);
+            const second = phone.toString().slice(3, 7);
+            const third = phone.toString().slice(7);
             const newItem = {
                 _id,
                 first_name,
