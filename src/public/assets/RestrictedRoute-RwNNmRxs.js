4eea2019f0ccec5a212192c5c7cd5702ffec1235
Santiago Rivarola <srivarola@gmail.com>
2024-01-28 20:34:42 -0300

    build

---
+++ src/public/assets/RestrictedRoute-RwNNmRxs.js
@@ -0,0 +1,1 @@
+import{f as o,j as s,N as r}from"./index-JWa7NUy4.js";const i=({children:t})=>{const{adminRole:e}=o();return e?t:s.jsx(r,{to:"/login"})};export{i as default};
---
+++ src/public/assets/chunk-KARN4QIT-1QmJr7SI.js
@@ -0,0 +1,1 @@
+import{aW as t}from"./index-JWa7NUy4.js";var i=(r,e)=>t(r)?r instanceof Array?[...r]:r[e]:r;export{i as g};
