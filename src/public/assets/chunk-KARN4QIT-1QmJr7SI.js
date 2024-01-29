7bbb2161e4966d3ccd934f1c07f1df95a4888e35
Santiago Rivarola <srivarola@gmail.com>
2024-01-29 09:04:57 -0300

    build

--- src/public/assets/chunk-KARN4QIT-1QmJr7SI.js
+++
@@ -1,1 +0,0 @@
-import{aW as t}from"./index-JWa7NUy4.js";var i=(r,e)=>t(r)?r instanceof Array?[...r]:r[e]:r;export{i as g};
--- src/public/assets/pen-square-wP5Kru1g.js
+++
@@ -1,6 +0,0 @@
-import{W as m,aU as u,Q as i,j as p,af as x,e as v}from"./index-JWa7NUy4.js";var e=m((o,s)=>{const{as:t,children:r,className:l,...n}=o,{slots:c,classNames:a}=u(),d=i(s),f=t||"footer";return p.jsx(f,{ref:d,className:c.footer({class:x(a==null?void 0:a.footer,l)}),...n,children:r})});e.displayName="NextUI.ModalFooter";var h=e;/**
- * @license lucide-react v0.294.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const N=v("PenSquare",[["path",{d:"M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7",key:"1qinfi"}],["path",{d:"M18.5 2.5a2.12 2.12 0 0 1 3 3L12 15l-4 1 1-4Z",key:"w2jsv5"}]]);export{N as P,h as m};
