f0308089096e48a9e068443418147b17dfce67cd
Santiago Rivarola <srivarola@gmail.com>
2024-02-03 20:33:53 -0300

    build

--- src/public/assets/pen-square-P6icTtnP.js
+++
@@ -1,6 +0,0 @@
-import{a9 as m,aW as u,ao as i,j as p,a7 as x,g as v}from"./index-nggJnTEe.js";var o=m((e,s)=>{const{as:t,children:r,className:l,...n}=e,{slots:c,classNames:a}=u(),d=i(s),f=t||"footer";return p.jsx(f,{ref:d,className:c.footer({class:x(a==null?void 0:a.footer,l)}),...n,children:r})});o.displayName="NextUI.ModalFooter";var h=o;/**
- * @license lucide-react v0.294.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const N=v("PenSquare",[["path",{d:"M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7",key:"1qinfi"}],["path",{d:"M18.5 2.5a2.12 2.12 0 0 1 3 3L12 15l-4 1 1-4Z",key:"w2jsv5"}]]);export{N as P,h as m};
