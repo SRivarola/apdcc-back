27ff9d6caa2288bc32d7fd260880065a834683eb
Santiago Rivarola <srivarola@gmail.com>
2024-02-04 10:58:18 -0300

    build

---
+++ src/public/assets/RestrictedRoute-95NiWA-w.js
@@ -0,0 +1,1 @@
+import{k as o,j as s,N as r}from"./index-6sd4aO59.js";const i=({children:t})=>{const{adminRole:e}=o();return e?t:s.jsx(r,{to:"/login"})};export{i as default};
---
+++ src/public/assets/chunk-KARN4QIT-x6RZkLzl.js
@@ -0,0 +1,1 @@
+import{aZ as t}from"./index-6sd4aO59.js";var i=(r,e)=>t(r)?r instanceof Array?[...r]:r[e]:r;export{i as g};
