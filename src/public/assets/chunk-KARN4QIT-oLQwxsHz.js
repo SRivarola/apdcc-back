9514a142f102e967249314a8c90c6c646fe6d665
Santiago Rivarola <srivarola@gmail.com>
2024-02-14 20:34:17 -0300

    build

--- src/public/assets/chunk-KARN4QIT-oLQwxsHz.js
+++
@@ -1,1 +0,0 @@
-import{aY as t}from"./index-jfKeHs1S.js";var i=(r,e)=>t(r)?r instanceof Array?[...r]:r[e]:r;export{i as g};
---
+++ src/public/assets/chunk-QY5NICTW-zOYmEfWT.js
@@ -0,0 +1,1 @@
+import{x as d,aV as x,y as p,j as u,B as i}from"./index-rfv_Nrgn.js";var s=d((a,e)=>{const{as:t,children:r,className:l,...f}=a,{slots:m,classNames:o}=x(),n=p(e),c=t||"footer";return u.jsx(c,{ref:n,className:m.footer({class:i(o==null?void 0:o.footer,l)}),...f,children:r})});s.displayName="NextUI.ModalFooter";var M=s;export{M as m};
